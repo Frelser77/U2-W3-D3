c8aaad7c10ae814c6b300a20697a75589fa1058c
Frelser77 <damicosalvatorealessandro@gmail.com>
2023-12-06 18:29:22 +0100

    Commit

--- asset/js/script.js
+++ asset/js/script.js
@@ -1,63 +1,29 @@
 document.addEventListener("DOMContentLoaded", function () {
 	const container = document.querySelector("#book-container"); // Seleziona l'elemento corretto
-	const carrello = []; // Inizializza un array vuoto per il carrello
+	let carrello = JSON.parse(localStorage.getItem("carrello")) || [];
 
-	// Funzione per aggiungere un libro al carrello
-	function aggiungiAlCarrello(libro) {
-		carrello.push(libro);
-		localStorage.setItem("carrello", JSON.stringify(carrello)); // Salva il carrello nel localStorage
-
-		// Aggiorna il carrello
+	function ripristinaCarrello() {
 		const listCarr = document.getElementById("lista-carrello");
-		const carrElem = document.createElement("li");
-		carrElem.className = "list-group-item d-flex justify-content-between align-items-center my-1 rounded-2 ";
-		carrElem.textContent = `${libro.title} - €${libro.price}`;
+		listCarr.innerHTML = ""; // Svuota qualsiasi contenuto esistente
 
-		// tasto rimuovi nel carrello
-		const btnRemove = document.createElement("button");
-		btnRemove.className = "btn btn-danger btn-sm px-3 py-1";
-		btnRemove.textContent = "Rimuovi";
-		btnRemove.onclick = function () {
-			rimuoviDalCarrello(libro, carrElem);
-		};
+		carrello.forEach((libro) => {
+			const carrElem = document.createElement("li");
+			carrElem.className = "list-group-item d-flex justify-content-between align-items-center my-1 rounded-2 ";
+			carrElem.textContent = `${libro.title} - €${libro.price}`;
 
-		carrElem.appendChild(btnRemove);
-		listCarr.appendChild(carrElem);
+			const btnRemove = document.createElement("button");
+			btnRemove.className = "btn btn-danger btn-sm px-3 py-1";
+			btnRemove.textContent = "Rimuovi";
+			btnRemove.onclick = function () {
+				rimuoviDalCarrello(libro, carrElem);
+			};
+
+			carrElem.appendChild(btnRemove);
+			listCarr.appendChild(carrElem);
+		});
 	}
 
-	function funClear() {
-		document.getElementById("lista-carrello").innerHTML = ""; // Svuota la lista del carrello nel DOM
-	}
-
-	// Creo un div che fungerà da contenitore per il pulsante
-	const divContainer = document.createElement("div");
-	divContainer.className = "text-center mt-3"; // Aggiungi le classi per centraggio e margin-top
-
-	// Creo il btn per svuotare il carrello
-	const btnSvuota = document.createElement("button");
-	btnSvuota.className = "btn btn-warning btn-sm fw-bold px-3 py-1";
-	btnSvuota.textContent = "Svuota Carrello";
-	btnSvuota.onclick = funClear;
-
-	// Aggiungo il btn al div contenitore
-	divContainer.appendChild(btnSvuota);
-
-	// Aggiungo il div contenitore sotto la lista del carrello
-	document.getElementById("carrello").appendChild(divContainer);
-
-	// rimuovere dal carrello il libro
-	function rimuoviDalCarrello(libroDaRimuovere, elementoCarrello) {
-		// Rimuove il libro dall'array carrello
-		const indice = carrello.indexOf(libroDaRimuovere);
-		if (indice > -1) {
-			carrello.splice(indice, 1);
-		}
-
-		// Aggiorna il localStorage
-		localStorage.setItem("carrello", JSON.stringify(carrello));
-		// Rimuove l'elemento dalla lista nel DOM
-		elementoCarrello.remove();
-	}
+	ripristinaCarrello(); // Questa linea va aggiunta
 
 	// Funzione per creare una card di libro
 	function creaCard(libro) {
@@ -87,14 +53,14 @@ document.addEventListener("DOMContentLoaded", function () {
 		const btnScarta = document.createElement("button"); // creo un bottono con classe e contenuto con un evento onclick per rimuovere il div.col
 		btnScarta.className = "btn btn-danger px-3 py-1";
 		btnScarta.textContent = "Scarta";
-		btnScarta.onclick = function () {
+		btnScarta.onclick = () => {
 			col.remove(); // Rimuove la card dalla pagina
 		};
 
 		const btnCompra = document.createElement("button"); // creo un bottono con classe e contenuto con un evento onclick per aggiungere al carrello il libro
 		btnCompra.className = "btn btn-success mx-2 px-3 py-1";
 		btnCompra.textContent = "Compra ora";
-		btnCompra.onclick = function () {
+		btnCompra.onclick = () => {
 			aggiungiAlCarrello(libro); // Aggiunge il libro al carrello
 		};
 
@@ -110,13 +76,69 @@ document.addEventListener("DOMContentLoaded", function () {
 		return col;
 	}
 
+	// Funzione per aggiungere un libro al carrello
+	function aggiungiAlCarrello(libro) {
+		carrello.push(libro);
+		localStorage.setItem("carrello", JSON.stringify(carrello)); // Salva il carrello nel localStorage
+
+		// Aggiorna il carrello
+		const listCarr = document.getElementById("lista-carrello");
+		const carrElem = document.createElement("li");
+		carrElem.className = "list-group-item d-flex justify-content-between align-items-center my-1 rounded-2 ";
+		carrElem.textContent = `${libro.title} - €${libro.price}`;
+
+		// tasto rimuovi nel carrello
+		const btnRemove = document.createElement("button");
+		btnRemove.className = "btn btn-danger btn-sm px-3 py-1";
+		btnRemove.textContent = "Rimuovi";
+		btnRemove.onclick = function () {
+			rimuoviDalCarrello(libro, carrElem);
+		};
+
+		carrElem.appendChild(btnRemove);
+		listCarr.appendChild(carrElem);
+	}
+
+	function funClear() {
+		carrello.length = 0;
+		localStorage.setItem("carrello", JSON.stringify(carrello));
+		document.getElementById("lista-carrello").innerHTML = "";
+	}
+
+	// Creo un div che fungerà da contenitore per il pulsante
+	const divContainer = document.createElement("div");
+	divContainer.className = "text-center mt-3"; // Aggiungi le classi per centraggio e margin-top
+
+	// Creo il btn per svuotare il carrello
+	const btnSvuota = document.createElement("button");
+	btnSvuota.className = "btn btn-warning btn-sm fw-bold px-3 py-1";
+	btnSvuota.textContent = "Svuota Carrello";
+	btnSvuota.onclick = funClear;
+
+	// Aggiungo il btn al div contenitore
+	divContainer.appendChild(btnSvuota);
+
+	// Aggiungo il div contenitore sotto la lista del carrello
+	document.getElementById("carrello").appendChild(divContainer);
+
+	// rimuovere dal carrello il libro
+	function rimuoviDalCarrello(libroDaRimuovere, elementoCarrello) {
+		// Rimuove il libro dall'array carrello
+		const indice = carrello.indexOf(libroDaRimuovere);
+		if (indice > -1) {
+			carrello.splice(indice, 1);
+		}
+		localStorage.setItem("carrello", JSON.stringify(carrello));
+		elementoCarrello.remove();
+	}
+
 	// Carico i libri dall'API e creo le card
 	fetch("https://striveschool-api.herokuapp.com/books")
 		.then((response) => {
 			if (response.ok) {
 				return response.json(); // Continua solo se la risposta è valida
 			} else {
-				console.error("Network response ok.");
+				console.error("Network response was not ok.");
 				return []; // Ritorna un array vuoto in caso di risposta non valida
 			}
 		})
